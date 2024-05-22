e3c55c44082702f379d4934fc1cbfc64308457ef
Jannik Hoefener <jannikhoefener@MacBook-Pro-von-Jannik.local>
2024-05-22 11:56:40 +0200

    first commit

--- RemindMeApp/app/(tabs)/index.tsx
+++ RemindMeApp/app/(tabs)/index.tsx
@@ -1,13 +1,91 @@
 import { StyleSheet } from "react-native";
-import { Box, View, Text, Switch, HStack } from "./../../components";
+import {
+  Box,
+  View,
+  Text,
+  Switch,
+  HStack,
+  Heading,
+  Accordion,
+  ChevronUpIcon,
+  ChevronDownIcon,
+  AccordionItem,
+  AccordionHeader,
+  AccordionContent,
+  AccordionContentText,
+  AccordionIcon,
+  AccordionTitleText,
+  AccordionTrigger,
+} from "./../../components";
+
+const dummyData: string[] = ["Aufgabe 1", "Aufgabe 2"];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Box bg="$primary500" p="$5"><Switch size="md" isDisabled={false} />
-        <HStack space="md" reversed={false} >
-          <Text color="white" >This is the Box</Text>
-        </HStack>
+      <Box>
+        <Heading size={"2xl"}>My Reminder</Heading>
+      </Box>
+      <Box bg="$primary500" p="$5">
+        {dummyData?.map((toDo, index) => {
+          return (
+            <Accordion m="$5" width="90%" size="md" variant="filled" type="single" isCollapsible={true} isDisabled={false} >
+              <AccordionItem value="a">
+                <AccordionHeader>
+                  <AccordionTrigger>
+                    {({ isExpanded }) => {
+                      return (
+                        <>
+                          <AccordionTitleText>
+                            How do I place an order?
+                          </AccordionTitleText>
+                          {isExpanded ? (
+                            <AccordionIcon as={ChevronUpIcon} ml="$3"/>
+                          ) : (
+                            <AccordionIcon as={ChevronDownIcon} ml="$3"/>
+                          )}
+                        </>
+                      );
+                    }}
+                  </AccordionTrigger>
+                </AccordionHeader>
+                <AccordionContent>
+                  <AccordionContentText>
+                    To place an order, simply select the products you want, proceed to
+                    checkout, provide shipping and payment information, and finalize
+                    your purchase.
+                  </AccordionContentText>
+                </AccordionContent>
+              </AccordionItem>
+               <AccordionItem value="b">
+                <AccordionHeader>
+                  <AccordionTrigger>
+                    {({ isExpanded }) => {
+                      return (
+                        <>
+                          <AccordionTitleText>
+                            What payment methods do you accept?
+                          </AccordionTitleText>
+                          {isExpanded ? (
+                            <AccordionIcon as={ChevronUpIcon} ml="$3"/>
+                          ) : (
+                            <AccordionIcon as={ChevronDownIcon} ml="$3"/>
+                          )}
+                        </>
+                      );
+                    }}
+                  </AccordionTrigger>
+                </AccordionHeader>
+                <AccordionContent>
+                  <AccordionContentText>
+                    We accept all major credit cards, including Visa, Mastercard, and
+                    American Express. We also support payments through PayPal.
+                  </AccordionContentText>
+                </AccordionContent>
+              </AccordionItem>
+              </Accordion>
+            );
+        })}
       </Box>
     </View>
   );
