96a743a1848d5b8eb601ab496d4372c036c9530e
Jannik Hoefener <jannikhoefener@MacBook-Pro-von-Jannik.local>
2024-05-23 14:13:14 +0200

    type definition and start project

--- RemindMeApp/app/(tabs)/index.tsx
+++ RemindMeApp/app/(tabs)/index.tsx
@@ -37,7 +37,7 @@ export default function TabOneScreen() {
                       return (
                         <>
                           <AccordionTitleText>
-                            How do I place an order?
+                            {toDo}
                           </AccordionTitleText>
                           {isExpanded ? (
                             <AccordionIcon as={ChevronUpIcon} ml="$3"/>
@@ -54,32 +54,6 @@ export default function TabOneScreen() {
                     To place an order, simply select the products you want, proceed to
                     checkout, provide shipping and payment information, and finalize
                     your purchase.
-                  </AccordionContentText>
-                </AccordionContent>
-              </AccordionItem>
-               <AccordionItem value="b">
-                <AccordionHeader>
-                  <AccordionTrigger>
-                    {({ isExpanded }) => {
-                      return (
-                        <>
-                          <AccordionTitleText>
-                            What payment methods do you accept?
-                          </AccordionTitleText>
-                          {isExpanded ? (
-                            <AccordionIcon as={ChevronUpIcon} ml="$3"/>
-                          ) : (
-                            <AccordionIcon as={ChevronDownIcon} ml="$3"/>
-                          )}
-                        </>
-                      );
-                    }}
-                  </AccordionTrigger>
-                </AccordionHeader>
-                <AccordionContent>
-                  <AccordionContentText>
-                    We accept all major credit cards, including Visa, Mastercard, and
-                    American Express. We also support payments through PayPal.
                   </AccordionContentText>
                 </AccordionContent>
               </AccordionItem>
--- RemindMeApp/types/reminder.ts
+++ RemindMeApp/types/reminder.ts
@@ -1,17 +1,33 @@
 // 1. first of all
-type RoutinePlanned = {
-    title: string
-    Repeats: {
-        //todo
-        day: string
-        time: string // 00:00 - 23:59
-    }[]
-    weather: {
-        location: string
-        weatherConditions: "sun" | "rain" | "snow" | string //todo: or string[] ... list if we like to have more then 1 conditions enableable
-    }
-    temperature: {
-        minC: number //or string?
-        maxC: number //or string?
-    }
+type week = {
+  Monday: boolean;
+  Tuesday: boolean;
+  Wednesday: boolean;
+  Thursday: boolean;
+  Friday: boolean;
+  Saturday: boolean;
+  Sunday: boolean;
+};
+
+type gpsCoords = {
+    latitude: number
+    longitude: number
 }
+export type RoutinePlanned = {
+  message: string;
+  isActive: boolean;
+  Repeats: {
+    time: string; 
+    // maybe more than one time to remind more than one time per day 
+    // maybe random time which is invisible to create random reminder
+    days: week;
+  };
+  weather: {
+    location: gpsCoords;
+    weatherConditions: "Clear"|"sun" | "rain" | "snow" | string; //todo: or string[] ... list if we like to have more then 1 conditions enableable
+  };
+  temperature: {
+    celisus: number; //or string?
+    isMin: boolean; //else true isMin ... false isMay
+  };
+};
