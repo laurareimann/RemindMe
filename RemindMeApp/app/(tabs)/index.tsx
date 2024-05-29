4d5a9f0f9f17ec61916240a7ee9320a24e4add29
Jannik Hoefener <jannik.hoefener@haw-hamburg.de>
2024-05-29 09:33:52 +0200

    examples, isActive, icons, delte

--- RemindMeApp/app/(tabs)/index.tsx
+++ RemindMeApp/app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import {
   Box,
   View,
@@ -16,59 +15,158 @@ import {
   AccordionIcon,
   AccordionTitleText,
   AccordionTrigger,
+  Icon,
+  VStack,
+  SunIcon,
+  ClockIcon,
+  ScrollView,
+  Button,
+  TrashIcon,
 } from "./../../components";
-
-const dummyData: string[] = ["Aufgabe 1", "Aufgabe 2"];
+import { PlannedReminderDummyData } from "@/types/reminder";
+import { useState } from "react";
+import PageView from "@/custom-components/templates";
+import { Alert } from "react-native";
 
 export default function TabOneScreen() {
+  const [routines, setRoutines] = useState(PlannedReminderDummyData);
+
+  const handleSwitchToggle = (index: number) => {
+    // Use the map function to create a new array of routines with the updated isActive value
+    const updatedRoutines = routines.map((routine, i) =>
+      // Check if the current routine's index matches the index of the toggled switch
+      i === index ? { ...routine, isActive: !routine.isActive } : routine
+    );
+    // Update the state with the new array of routines
+    setRoutines(updatedRoutines);
+  };
+  const handleDeleteRoutine = (index: number) => {
+    Alert.alert(
+      "Delete Routine",
+      "Are you sure you want to delete this routine?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            const updatedRoutines = routines.filter((_, i) => i !== index);
+            setRoutines(updatedRoutines);
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Box>
-        <Heading size={"2xl"}>My Reminder</Heading>
+    <PageView>
+      <Box pt="$5">
+        <Heading size={"2xl"}>Remind Me</Heading>
       </Box>
-      <Box bg="$primary500" p="$5">
-        {dummyData?.map((toDo, index) => {
-          return (
-            <Accordion m="$5" width="90%" size="md" variant="filled" type="single" isCollapsible={true} isDisabled={false} >
-              <AccordionItem value="a">
-                <AccordionHeader>
-                  <AccordionTrigger>
-                    {({ isExpanded }) => {
-                      return (
-                        <>
-                          <AccordionTitleText>
-                            {toDo}
-                          </AccordionTitleText>
-                          {isExpanded ? (
-                            <AccordionIcon as={ChevronUpIcon} ml="$3"/>
-                          ) : (
-                            <AccordionIcon as={ChevronDownIcon} ml="$3"/>
-                          )}
-                        </>
-                      );
-                    }}
-                  </AccordionTrigger>
-                </AccordionHeader>
-                <AccordionContent>
-                  <AccordionContentText>
-                    To place an order, simply select the products you want, proceed to
-                    checkout, provide shipping and payment information, and finalize
-                    your purchase.
-                  </AccordionContentText>
-                </AccordionContent>
-              </AccordionItem>
+      <Box pt="$5">
+        <Heading size={"lg"}>My Routines</Heading>
+        <ScrollView style={{ flexGrow: 1}}>
+          {routines?.map((toDo, index) => {
+            return (
+              <Accordion
+                key={index}
+                m="$2.5"
+                width="90%"
+                size="md"
+                variant="filled"
+                type="single"
+              >
+                <AccordionItem value={index.toString()}>
+                  <AccordionHeader>
+                    <AccordionTrigger>
+                      {({ isExpanded }) => {
+                        const statefulColor = toDo.isActive ? "black" : "grey";
+                        return (
+                          <>
+                            <Icon
+                              as={toDo.icon}
+                              mr="$1.5"
+                              color={statefulColor}
+                            />
+                            <AccordionTitleText color={statefulColor}>
+                              {toDo.message}
+                            </AccordionTitleText>
+                            {isExpanded ? (
+                              <AccordionIcon
+                                as={ChevronUpIcon}
+                                color={statefulColor}
+                              />
+                            ) : (
+                              <AccordionIcon
+                                as={ChevronDownIcon}
+                                color={statefulColor}
+                              />
+                            )}
+                            <Switch
+                              size="sm"
+                              value={toDo.isActive}
+                              onToggle={() =>
+                                handleSwitchToggle(index)
+                              } /* todo: onToggle={} ... manipulate isActive value */
+                            />
+                          </>
+                        );
+                      }}
+                    </AccordionTrigger>
+                  </AccordionHeader>
+                  <AccordionContent>
+                    <VStack space="sm" p={3}>
+                      {/* Repeats Section */}
+                      {/* Todo: ReWork this here */}
+                      <HStack alignItems="center" space="sm">
+                        <Button onPress={() => handleDeleteRoutine(index)}>
+                          <Icon as={TrashIcon} color="red" />
+                        </Button>
+                        <Icon as={ClockIcon} size="sm" />
+                        <Text>
+                          {Array.isArray(toDo.Repeats.time)
+                            ? toDo.Repeats.time.join(", ")
+                            : toDo.Repeats.time}
+                        </Text>
+                      </HStack>
+                      <Text>
+                        {Object.keys(toDo.Repeats.days)
+                          .filter((day) => toDo.Repeats.days[day])
+                          .join(", ")}
+                      </Text>
+
+                      {/* Weather Section */}
+                      {/* Todo: ReWork this here */}
+                      <HStack alignItems="center" space="sm">
+                        <Icon as={SunIcon} size="sm" />
+                        <Text>
+                          {Array.isArray(toDo.weather.weatherConditions)
+                            ? toDo.weather.weatherConditions.join(", ")
+                            : toDo.weather.weatherConditions}
+                        </Text>
+                      </HStack>
+                      <Text>
+                        {toDo.temperature.isMin ? "min" : "max"}{" "}
+                        {toDo.temperature.celsius}°C
+                      </Text>
+                    </VStack>
+                  </AccordionContent>
+                </AccordionItem>
               </Accordion>
             );
-        })}
+          })}
+        </ScrollView>
       </Box>
-    </View>
+      {/* Why does this not appear */}
+      <Box>
+        <Heading size={"lg"}>where button?!</Heading>
+        <Button>
+          <Text>test</Text>
+        </Button>
+      </Box>
+    </PageView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
